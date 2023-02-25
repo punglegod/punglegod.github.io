aacbb45cc843a1a73cb135ec06b2d40a54ab46a6
punglegod <2301201756@qq.com>
2023-02-25 22:51:50 +0800

    Site updated: 2023-02-25 22:51:49

--- js/txmap.js
+++ js/txmap.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(113.34499552, 23.15537143, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(106.64188, 30.47392, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
